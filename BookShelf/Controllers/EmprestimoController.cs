8b2cc423fa1cdb0e1da887582d9421cf5be9799d
Felipe Brandao <brandao154@outlook.com>
2024-04-23 21:53:58 -0300

    frontEnd modifications and added some data to show

---
+++ BookShelf/Controllers/EmprestimoController.cs
@@ -0,0 +1,22 @@
+﻿using BookShelf.Data;
+using BookShelf.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShelf.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+        readonly private ApplicationDbContext _db;
+        public EmprestimoController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<EmprestimosModel> emprestimos = _db.Emprestimos;
+            return View(emprestimos);
+        }
+    }
+}
